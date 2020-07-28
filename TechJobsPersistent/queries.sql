41d41d9c89d3a80007dfca4e216be1b373c0b7fb
rhagen87 <reynold.hagen@gmail.com>
2020-07-28 14:39:06 -0500

    assignment complete

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -3,6 +3,11 @@ select COLUMN_NAME, DATA_TYPE
 from INFORMATION_SCHEMA.COLUMNS 
 where TABLE_NAME='jobs';
 --Part 2
-
+SELECT Name
+FROM employers
+WHERE Location LIKE "St. Louis%"
 --Part 3
-
+SELECT Name, Description
+FROM skills
+INNER JOIN jobskills ON  skills.Id = jobskills.SkillId
+WHERE jobskills.JobId IS NOT NULL
